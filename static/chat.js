1db3f3026fedaadaf0617ed71bbec66981a8aba8
Arman Ashrafian <ashy63@gmail.com>
2018-02-24 01:42:00 -0800

    adding materialize styles

--- static/chat.js
+++ static/chat.js
@@ -9,33 +9,13 @@ window.onload = function () {
     let websocketurl = (DEBUG ?
         "ws://" : "wss://") + document.location.host + "/ws";
 
-    function appendLog(item) {
-        let doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-        log.appendChild(item);
-        if (doScroll) {
-            log.scrollTop = log.scrollHeight - log.clientHeight;
+    /* Set events for send click & enter pressed */
+    $("#sendButton").click(sendMessage)
+    $("#chatMsg").keypress((e) => {
+        if (e.keyCode === 13) {
+            sendMessage();
         }
-    }
-
-    /* SEND MESSAGE EVENT */
-    document.getElementById("form").onsubmit = function () {
-        if (!conn) {
-            return false;
-        }
-        if (!msg.value) {
-            return false;
-        }
-        if (screenname == "") {
-            promptUserForScreenName()
-        }
-        let message = {
-            message: msg.value,
-            from: screenname
-        }
-        conn.send(JSON.stringify(message));
-        msg.value = ""; // clear message input field
-        return false;
-    };
+    })
 
     if (window["WebSocket"]) {
         conn = new WebSocket(websocketurl);
@@ -48,12 +28,7 @@ window.onload = function () {
         conn.onmessage = function (evt) {
             let messages = evt.data.split('\n');
             for (let i = 0; i < messages.length; i++) {
-                let message = JSON.parse(messages[i])
-                let item = document.createElement("div");
-
-                // display message
-                item.innerHTML = `<b>${message.from}</b>: ${message.message}`;
-                appendLog(item);
+                displayMessage(messages[i]);
             }
         };
     } else {
@@ -64,5 +39,43 @@ window.onload = function () {
 
     function promptUserForScreenName() {
         screenname = prompt("Enter screen name ya dum bitch");
+    }
+
+    function displayMessage(mes) {
+        let message = JSON.parse(mes)
+        let item = document.createElement("div");
+
+        // display message
+        item.innerHTML = `<b>${message.from}</b>: ${message.message}`;
+        appendLog(item);
+    }
+
+    function sendMessage() {
+        let msg = $("#chatMsg");
+
+        if (!conn) {
+            return false;
+        }
+        if (!msg.val()) {
+            return false;
+        }
+        if (screenname == "") {
+            promptUserForScreenName()
+        }
+        let message = {
+            message: msg.val(),
+            from: screenname
+        }
+        conn.send(JSON.stringify(message));
+        msg.val(""); // clear message input field
+        return false;
+    }
+
+    function appendLog(item) {
+        let doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+        log.appendChild(item);
+        if (doScroll) {
+            log.scrollTop = log.scrollHeight - log.clientHeight;
+        }
     }
 };
